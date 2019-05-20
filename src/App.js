1c1cb945321a92d0671e4173aa5a6a2b3df1ad38
andre1828 <andremelo13@gmail.com>
2019-05-19 23:09:20 -0300

    transfers logic to new Service class

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
 import "./App.css"
-import Service from "./Service.js"
+import Service from "./Service/Service"
 
 class App extends React.Component {
   constructor(props) {
