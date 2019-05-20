34e1c056d20f1b5981d9fd9172a051333437081e
andre1828 <andremelo13@gmail.com>
2019-05-19 23:19:26 -0300

    Implements interface

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import "./App.css"
+import "./flaticon.css"
 import Service from "./Service/Service"
 
 class App extends React.Component {
@@ -13,16 +14,23 @@ class App extends React.Component {
     this.state = {
       images: [],
       stickerizedImages: [],
-      finalFile: ''
+      finalFile: "",
     }
   }
 
   handleFilePickerChanged(files) {
-    this.setState({ images: [...this.state.images, ...files] })
+    this.setState({
+      images: [...this.state.images, ...files],
+      mainSectionContent: "imagesList"
+    })
   }
 
   handleDownloadButtonClicked() {
     window.location = this.state.finalFile
+  }
+
+  handleRemoveButtonClicked(index) {
+    this.setState({ images: this.state.images.filter((_, i) => i !== index) })
   }
 
   async stickerizeAll() {
@@ -33,16 +41,78 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <h3>STICKERIZE</h3>
+        <section className="main-section">
+          {!this.state.images.length ? (
+            <label htmlFor="imageInput">
+              <div className="picture-icon" />
+              <p>SELECT IMAGES</p>
+            </label>
+          ) : (
+            <div className="imageList">
+              {this.state.images.map((img, index) => (
+                <div className="image">
+                  <img src={require("./dont_panic.png")} alt=" " />
+                  <div className="devider-vertical" />
+                  <span className="image-name">dont panic.png</span>
+                  <div
+                    className="image-remove-icon"
+                    onClick={() => this.handleRemoveButtonClicked(index)}
+                  />
+                </div>
+              ))}
+            </div>
+          )}
+        </section>
+        <div className="devider" />
+        <section className="feature-container">
+          <div className="feature">
+            <div className="feature-icon layers" />
+            <p>Process hundreds of images at once</p>
+          </div>
+          <div className="feature">
+            <div className="feature-icon stop-watch-4" />
+            <p>No downloads, choose your images and get going right away</p>
+          </div>
+        </section>
         <input
+          id={"imageInput"}
           type="file"
           multiple={true}
           accept={"image/jpeg,image/png,image/gif"}
           onChange={e => this.handleFilePickerChanged(e.target.files)}
+          style={{
+            visibility: "hidden"
+          }}
         />
-        <button onClick={() => this.stickerizeAll()}>Stickerize</button>
+        <footer>
+          <div>
+            Icons made by
+            <a
+              href="https://www.flaticon.com/authors/smashicons"
+              title="Smashicons"
+            >
+              Smashicons
+            </a>
+            from
+            <a href="https://www.flaticon.com/" title="Flaticon">
+              www.flaticon.com
+            </a>
+            is licensed by
+            <a
+              href="http://creativecommons.org/licenses/by/3.0/"
+              title="Creative Commons BY 3.0"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              CC 3.0 BY
+            </a>
+          </div>
+        </footer>
+        {/* <button onClick={() => this.stickerizeAll()}>Stickerize</button>
         <button onClick={() => this.handleDownloadButtonClicked()}>
           Download
-        </button>
+        </button> */}
       </div>
     )
   }
