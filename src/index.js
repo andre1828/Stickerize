926538030fdf2760dd132803e3656f36a8f488c7
andre1828 <andremelo13@gmail.com>
2019-06-05 14:06:49 -0300

    Add react-router-dom and move image list to it's own component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDOM from "react-dom"
+import { BrowserRouter, Route } from "react-router-dom"
+import "./index.css"
+import App from "./App"
+import ImagesList from "./../src/ImageList/ImageList"
+import * as serviceWorker from "./serviceWorker"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+      <Route exact path="/" component={App} />
+      <Route path="/images" component={ImagesList} />
+  </BrowserRouter>,
+  document.getElementById("root")
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
